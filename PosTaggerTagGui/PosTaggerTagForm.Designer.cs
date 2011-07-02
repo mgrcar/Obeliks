802c7b23e8fe7dd89075b0edaac4f69d95d29abd
mgrcar <miha.grcar@ijs.si>
2011-07-02 12:55:16 +0000

    PosTaggerTagGui first commit. PosTaggerTagLib created.
    git-svn-id: svn://first.ijs.si:3691/postagger@263 35089d11-96e9-ae4d-82a0-4b2588c93174

---
+++ PosTaggerTagGui/PosTaggerTagForm.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace PosTaggerTagGui
+{
+    partial class PosTaggerTagForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.pnlStatus = new System.Windows.Forms.Panel();
+            this.pnlStatus.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.BackColor = System.Drawing.Color.Black;
+            this.txtStatus.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtStatus.Font = new System.Drawing.Font("Courier New", 8.150944F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStatus.ForeColor = System.Drawing.Color.Lime;
+            this.txtStatus.Location = new System.Drawing.Point(0, 0);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.ReadOnly = true;
+            this.txtStatus.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtStatus.Size = new System.Drawing.Size(806, 242);
+            this.txtStatus.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(169, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar.Location = new System.Drawing.Point(0, 242);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(806, 23);
+            this.progressBar.TabIndex = 2;
+            this.progressBar.Visible = false;
+            // 
+            // pnlStatus
+            // 
+            this.pnlStatus.Controls.Add(this.txtStatus);
+            this.pnlStatus.Controls.Add(this.progressBar);
+            this.pnlStatus.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlStatus.Location = new System.Drawing.Point(0, 312);
+            this.pnlStatus.Name = "pnlStatus";
+            this.pnlStatus.Size = new System.Drawing.Size(806, 265);
+            this.pnlStatus.TabIndex = 3;
+            // 
+            // PosTaggerTagForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 577);
+            this.Controls.Add(this.pnlStatus);
+            this.Controls.Add(this.button1);
+            this.Name = "PosTaggerTagForm";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.PosTaggerTagForm_FormClosing);
+            this.pnlStatus.ResumeLayout(false);
+            this.pnlStatus.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Panel pnlStatus;
+    }
+}
+
---
+++ PosTaggerTagGui/PosTaggerTagForm.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+using Latino;
+using PosTagger;
+
+namespace PosTaggerTagGui
+{
+    public partial class PosTaggerTagForm : Form
+    {
+        private Thread mThread
+            = null;
+
+        public PosTaggerTagForm()
+        {
+            InitializeComponent();
+            // initialize LATINO logger
+            Logger mLogger = Logger.GetRootLogger();
+            mLogger.LocalLevel = Logger.Level.Debug;
+            mLogger.LocalOutputType = Logger.OutputType.Custom;
+            mLogger.LocalProgressOutputType = Logger.ProgressOutputType.Custom;
+            mLogger.CustomOutput = new Logger.CustomOutputDelegate(delegate(string loggerName, Logger.Level level, string funcName, Exception e, string message, object[] msgArgs) 
+            {
+                try
+                {
+                    Invoke(new ThreadStart(delegate()
+                    {
+                        if (txtStatus.Text != "") { txtStatus.AppendText("\r\n"); }
+                        txtStatus.AppendText(string.Format(message, msgArgs));
+                        txtStatus.SelectionStart = txtStatus.TextLength;
+                        txtStatus.ScrollToCaret();
+                    }));
+                }
+                catch { }
+            });
+            mLogger.CustomProgressOutput = new Logger.CustomProgressOutputDelegate(delegate(string loggerName, object sender, int freq, string funcName, string message, int step, int numSteps, object[] args) 
+            {
+                try
+                {
+                    Invoke(new ThreadStart(delegate()
+                    {
+                        if (numSteps == step)
+                        {
+                            progressBar.Visible = false;
+                        }
+                        else
+                        {                            
+                            progressBar.Maximum = numSteps;
+                            progressBar.Value = step;
+                            progressBar.Visible = true;
+                        }
+                    }));
+                } 
+                catch { }
+            });
+        }
+
+        private void DisableForm()
+        { 
+        }
+
+        private void EnableForm()
+        { 
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            DisableForm();
+            txtStatus.Clear();
+            mThread = new Thread(new ThreadStart(delegate()
+            {
+                PosTaggerTag.Tag(new string[] { "-v", "-k", "-xml", @"-lem:C:\Work\PosTagger\Data\lemmatizer.bin", @"C:\Work\PosTagger\Data\jos100k-test.xml",
+                    @"C:\Work\PosTagger\Data\jos100k-train.bin", @"C:\Work\PosTagger\Data\output.xml" });
+                Invoke(new ThreadStart(delegate() { EnableForm(); }));
+            }));
+            mThread.Start();
+        }
+
+        private void PosTaggerTagForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                mThread.Abort();
+            }
+            catch { }
+        }
+    }
+}
