0e8ee40ca4e0349aa1d3a70ad09525eb5dc47ad0
mgrcar <miha.grcar@ijs.si>
2016-07-21 10:16:00 +0200

    Rmv obsolete projects

--- Latino/TextMining/LemmaSharp/ILemmatizerModel.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-namespace LemmaSharp {
-    public interface ILemmatizerModel {
-        string Lemmatize(string sWord);
-        string ToString();
-    }
-}
--- WebService/ObeliksService.asmx
+++
@@ -1,1 +0,0 @@
-<%@ WebService Language="C#" CodeBehind="~/App_Code/ObeliksService.cs" Class="ObeliksService" %>
